a3c1044410d657767193b15100b2e4e78e93d158
Pedro Yamamoto <pvyamamoto@gmail.com>
2024-09-11 17:53:45 -0300

    a

---
+++ src/class3.java
@@ -0,0 +1,12 @@
+public class class3 {
+    public static void main(String[] args){
+        int sum = 0;
+        int i = 12;
+        for(int k = 1 ; k <= i;){
+            k++;
+            sum += k;
+        }
+
+        System.out.println(sum);
+    }
+}
